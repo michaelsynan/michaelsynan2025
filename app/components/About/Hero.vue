8f1a91c6b2fd766be745d457813e613bc6538e55
Michael Synan <hello@michaelsynan.com>
2025-09-24 02:23:03 -0400

    Feat: fixing hero section

--- app/components/About/Hero.vue
+++ app/components/About/Hero.vue
@@ -16,9 +16,11 @@ const selectedTabComponent = computed(() => {
   <div class="flex flex-col justify-between items-start min-h-screen p-4 gap-8 md:gap-0">
     <div class="grid grid-cols-1 md:grid-cols-2 w-full flex-1 min-h-1/2">
       <div class="hidden md:block" />
-      <div class="flex flex-col justify-center h-full pt-20 md:pt-0">
-        <h1 class="text-3xl md:text-5xl mx-auto max-w-md leading-tight text-left">Hi, I'm Michael Synan. Pleased to meet
-          you.
+      <div class="flex flex-col justify-start md:justify-center h-full pt-20 md:pt-0 mr-auto md:mr-[unset]">
+        <h1 class="text-3xl md:text-5xl mx-auto max-w-md leading-tight text-left flex flex-col"><span>Hi, I'm Michael
+            Synan.</span><span>Pleased
+            to meet
+            you.</span>
         </h1>
       </div>
     </div>
